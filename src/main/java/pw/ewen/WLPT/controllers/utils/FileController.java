c60a94b307ad279ca56749879ac5246c2e1ea625
wenliang <ewen80@qq.com>
2022-01-19 13:04:02 +0800

    修改文件上传

--- src/main/java/pw/ewen/WLPT/controllers/utils/FileController.java
+++ src/main/java/pw/ewen/WLPT/controllers/utils/FileController.java
@@ -4,13 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pw.ewen.WLPT.configs.biz.BizConfig;
-import pw.ewen.WLPT.domains.DTOs.SignatureDTO;
 import pw.ewen.WLPT.services.FileService;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Base64;
 
 /**
  * 文件上传，删除等操作
@@ -21,6 +19,7 @@ public class FileController {
 
     private static class UploadReturn {
         public String name;
+        public String path;
         public String status;
         public String date;
     }
@@ -37,13 +36,14 @@ public class FileController {
     /**
      * 上传文件按照年月分文件夹存储
      * @param file 文件流
-     * @apiNote  {name: "filename", status: "result", date: "上传时间"}, result: "done"完成 "error"错误
+     * @apiNote  {name: "原始文件名", path: "含路径的文件名", status: "result", date: "上传时间"}, result: "done"完成 "error"错误
      */
     @PostMapping(value = "/upload", produces = "application/json")
     public UploadReturn upload(@RequestParam("file") MultipartFile file)  {
         UploadReturn uploadReturn = new UploadReturn();
         try{
-            uploadReturn.name = fileService.upload(file);
+            uploadReturn.name = file.getOriginalFilename();
+            uploadReturn.path = fileService.upload(file);
             uploadReturn.date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
             uploadReturn.status = "done";
         }catch (IOException e) {
